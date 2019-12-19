2d618a58d1db66c9f21492ed25c035ba7aecaf16
ykallan <45066189+ykallan@users.noreply.github.com>
2019-12-19 15:16:29 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,3 @@ from selenium import webdriver
 b = webdriver.PhantomJS()
 b.get('https://www.baidu.com')
 print(b.current_url)
-'''
-ssr://bWYxLnFxc3NyLnRvcDo1NzIwNjpvcmlnaW46cmM0LW1kNTpwbGFpbjpibkJ0VkVOTC8_b2Jmc3BhcmFtPSZwcm90b3BhcmFtPSZyZW1hcmtzPTU3MlI1NnVaNUxpSzZaMmk1TGlBNTV1MDVweUo1WVdONkxTNTU1cUVjM055Q25oNmMzTnlMblJ2Y0FybXM2amxob3pvdEtibGo3Zmt2Yl9ubEtnJmdyb3VwPTVhNlk1NzJST25oNmMzTnlMblJ2Y0E
-'''
